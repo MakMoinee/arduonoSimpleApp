e56348f2f81b7b2369852a1155e98f99273754e8
DESKTOP-LKF81KV\Mak Moinee <titus.silver.ph@gmail.com>
2023-03-08 17:32:36 +0800

    update code

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/sensordataget", function (req, res, next) {
+  console.log(req.query);
+  fetch(
+    "https://aqmrefresh.com/storeget.php?api_key=" +
+      req.query.api_key +
+      "&voc_value=" +
+      req.query.voc_value +
+      "&carbon_value=" +
+      req.query.voc_value +
+      ""
+  )
+    .then((response) => response.text())
+    .then((body) => {
+      res.send(body);
+    });
+
+});
+
+module.exports = router;
